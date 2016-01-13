65c77363070febb4a9edfff691a1127423df7f27
Simo YiN <yindp@outlook.com>
2016-01-13 11:20:45 +0800

    Design the layout.

--- app/src/main/java/com/yinom/rdc/colin/asynchttpclientdemo/LoginActivity.java
+++ app/src/main/java/com/yinom/rdc/colin/asynchttpclientdemo/LoginActivity.java
@@ -8,45 +8,33 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnSin = null;
+    EditText ettName = null;
+    EditText ettPwd = null;
+    TextView ttvTitle = null;
+    TextView ttvNotice = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        btnSin = (Button) findViewById(R.id.btnLgSin);
+        ettName = (EditText) findViewById(R.id.ettLgName);
+        ettPwd = (EditText) findViewById(R.id.ettLgPwd);
+        ttvTitle = (TextView) findViewById(R.id.ttvLgTitle);
+        ttvNotice = (TextView) findViewById(R.id.ttvLgNotice);
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
